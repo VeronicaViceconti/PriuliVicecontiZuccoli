6b98fafb76b113859c84e67c7e8360516b1040b1
Elia Priuli <elia@g15.>
2025-01-17 15:46:20 +0100

    firsts beans added

---
+++ src/main/java/it/polimi/se2/sandc/bean/Company.java
@@ -0,0 +1,21 @@
+package it.polimi.se2.sandc.bean;
+
+public class Company {
+	private String email; 
+	private String name;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/it/polimi/se2/sandc/bean/Match.java
@@ -0,0 +1,7 @@
+package it.polimi.se2.sandc.bean;
+
+public class Match{
+	private int id; 
+	private boolean accepted; 
+	
+}
